2da0d263dd7446f8e20d9f3e49ca3307ad0cb660
Adarsh Anand <adarshanshu7@gmail.com>
2024-05-19 23:43:31 +0530

    updated

--- portfolio/src/components/pages/Home.js
+++ portfolio/src/components/pages/Home.js
@@ -2,6 +2,7 @@ import {Component} from 'react';
 import '../styles/Home.css'
 import Avatar from '../static/avatar.jpg'
 import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { ReactTyped } from "react-typed";
 
 
@@ -19,6 +20,19 @@ class Home extends Component {
                 I am <ReactTyped className="sub-name-tag" strings={["coder 📜","developer 🖥️","content-creator 🎥"]} typeSpeed={80} backSpeed={40} loop/>
                 </p>
                 </Col>
+            </Row>
+            <Row className='align-items-center home-row-bio'>
+                <p className='text-section-bio'>
+                    <span className='home-row-bio-capitalized'>I</span> am a <a href="https://en.wikipedia.org/wiki/Hacker" target="_black">hacker</a> 👋🏻. Deeply curious about technology and social-engineering, I graduated from <a href="https://www.reva.edu.in/" target="_blank">Reva University</a> as an Engineer with a Bachelor's in computer science.
+                    <br /><br />
+                    I started developing websites at an early age for various startups, became a webmaster for various universities, IEEE, and IISc Bangalore events, and also wrote a research paper on <a href="http://www.testmagzine.biz/index.php/testmagzine/article/view/8343/6317" target='_blank'>Virtual Assitants based on User Preferences</a>
+                    <br /><br />
+                    I started my <i>career</i> as a frontend developer, but the latter of my curiosities learned about cloud technologies. Currently, I am seeking job oppurtunities as a full stack developer.
+                    <br /><br />
+                    When I am not in front of screen, I probably go to put my life on track and be the best version. I like cafes, music, wanderlust and creative. I will be sharing what I experience <Link to="/nontech">here</Link>
+                    <br /><br />
+                    Stay in touch! ✨<br /><br />
+                </p>
             </Row>
             </Container>
         );
