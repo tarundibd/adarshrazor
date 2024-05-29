85e2a7be056e2a7f6c5853d53783ee609e11fd20
Adarsh Anand <adarshanshu7@gmail.com>
2024-05-29 17:13:19 +0530

    updated

--- portfolio/src/components/pages/misc.js
+++ portfolio/src/components/pages/misc.js
@@ -1,9 +1,20 @@
 import { Component} from 'react'
+import Avatar from '../static/404.png'
+import { Image } from 'react-bootstrap';
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 class Misc extends Component {
     render() {
         return (
-            <h2 className='text-center m-4'>You have a lot of curiosity. Don't bush around - You don't have persmission yet to access this.</h2>
+            <div className='text-center'>
+                <Image src={Avatar} alt="404 Image" className='m-2' roundedCircle fluid style={{ width: '100%', maxWidth: '300px', height: '350px', borderRadius: '10px', alignContent: 'center' }}  /> 
+                <h2 className='m-1'>You have a lot of curiosity. You don't have persmission yet to access this page.</h2>
+                <h3>Play around some more !</h3>
+                <div className='mx-5 my-4'>
+                    <p style={{color:'red'}}>Loading ...</p>
+                    <ProgressBar striped variant="danger" now={80} />
+                </div>
+            </div>
         )
     }
 }
