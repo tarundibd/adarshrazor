73703f310cff8ac8e4414893d27ab7b1eac6f97e
Adarsh Anand <adarshanshu7@gmail.com>
2025-06-14 06:20:30 +0530

    suoduosdhng

--- portfolio/src/components/ui/animated-modal.tsx
+++ portfolio/src/components/ui/animated-modal.tsx
@@ -221,15 +221,15 @@ const CloseIcon = () => {
 // Add it in a separate file, I've added here for simplicity
 export const useOutsideClick = (
   ref: React.RefObject<HTMLDivElement>,
-  callback: Function
+  callback: () => void
 ) => {
   useEffect(() => {
-    const listener = (event: any) => {
+    const listener = (event: MouseEvent | TouchEvent) => {
       // DO NOTHING if the element being clicked is the target element or their children
-      if (!ref.current || ref.current.contains(event.target)) {
+      if (!ref.current || ref.current.contains(event.target as Node)) {
         return;
       }
-      callback(event);
+      callback();
     };
 
     document.addEventListener("mousedown", listener);
